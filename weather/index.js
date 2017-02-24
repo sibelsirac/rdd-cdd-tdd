3a0754640ff7ef60abcba22fed2a9d3ea364ee3e
sibelsirac <sibel.sirac@devinci.fr>
2017-02-24 14:30:21 +0100

    first comment on index.js

--- weather/index.js
+++ weather/index.js
@@ -9,8 +9,10 @@ module.exports = (opts, callback) => {
 	let query;
 
 	if (_.isEmpty(opts)) {
+	// the query by default without arguments
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Dhaka, Bangladesh")');
 	} else {
+	// the query with the given arguments
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + opts[0] + ', ' + opts[1] + '")');
 	}
 
